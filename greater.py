b3332b04bec606d5eb934c7a804ceed695fdd59f
hannah9250 <hannahmariam1912@gmail.com>
2026-04-27 17:08:19 +0400

    conditional statement

---
+++ greater.py
@@ -0,0 +1,5 @@
+num=int(input("enter a number: "))
+if num < 15: 
+    print("smaller")
+else: 
+    print("greater")
---
+++ profit.py
@@ -0,0 +1,6 @@
+costprice=int(input("enter cost price: "))
+sellingprice=int(input("enter selling price: "))
+if sellingprice>costprice: 
+    print("profit")
+else: 
+    print("loss")
