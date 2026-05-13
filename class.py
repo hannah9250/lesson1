c7249a24f52018b0b82fc48865d51448c9b180d0
hannah9250 <hannahmariam1912@gmail.com>
2026-05-13 16:05:11 +0400

    nested

---
+++ class.py
@@ -0,0 +1,8 @@
+age = int(input("enter your age: "))
+if age > 10: 
+    if age < 20: 
+        print("you can enroll")
+    else: 
+        print("u cant enroll,your age is more than 20")
+else: 
+    print("you cant enroll in class,your age is less than 10")
