d818cbecf2112fab35af4095c5a58807258b046c
hannah9250 <hannahmariam1912@gmail.com>
2026-06-10 18:00:38 +0400

    statement

---
+++ break.py
@@ -0,0 +1,7 @@
+word=input("enter a word: ")
+for letter in word: 
+    if letter == "a": 
+        print("yes it has been found!!")
+        break
+    else: 
+        print("it is not found")
---
+++ continue.py
@@ -0,0 +1,7 @@
+num=10
+while num > 0: 
+    if num == 5: 
+        num -= 1
+        continue
+    print(num)
+    num -= 1
